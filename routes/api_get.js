f00d55c01532f95481408b645dc735fa20da8319
lucaforgia <luca_forgia@yahoo.it>
2019-12-18 15:56:33 +0100

    fatto esaurimenti

--- routes/api_get.js
+++ routes/api_get.js
@@ -159,6 +159,7 @@ router.get('/search-filters', function(req, res, next) {
         res.send('error \n' + error);
     });
 });
+
 router.get('/contraptions/:id(\\d+)/', function(req, res, next) {
   let newData = {data:{}};
   req.magazutDb.any('SELECT * FROM contraption WHERE contraption_id=$1', req.params.id)
@@ -175,50 +176,61 @@ router.get('/contraptions/:id(\\d+)/', function(req, res, next) {
 router.get('/contraptions', function(req, res, next) {
   const queryRequest = req.query;
   let data = {data:[]};
-  let radiusSearch = queryRequest['geometry_radius'] ? ` AND geometry_radius = ${queryRequest['geometry_radius']}` : '';
-  let degreeSearch = queryRequest['geometry_degree'] ? ` AND geometry_degree = ${queryRequest['geometry_degree']}` : '';
-  let thicknessSearch = queryRequest['geometry_thickness'] ? ` AND geometry_thickness = ${queryRequest['geometry_thickness']}` : '';
-  let lengthSearch = queryRequest['geometry_length'] ? ` AND geometry_length = ${queryRequest['geometry_length']}` : '';
-  let diameterSearch = queryRequest['geometry_diameter'] ? ` AND geometry_diameter = ${queryRequest['geometry_diameter']}` : '';
-  let materialSearch = queryRequest['material'] ? ` AND material = ${queryRequest['material']}` : '';
-  let typeSearch = queryRequest['contraption_type'] ? ` AND type IN (${queryRequest['contraption_type']})` : '';
-  let machineSearch = queryRequest['machine'] ? ` AND machine IN (${queryRequest['machine']})` : '';
-  let idCodeSearch = queryRequest['id-code'] ? ` AND contraption.id_code LIKE '%${queryRequest["id-code"]}%'`: '';
-  let orderStatusSearch = queryRequest['order_status'] ? ` AND order_status = ${queryRequest['order_status']}` : '';
+  var sqlQuery = '';
+
+  if(queryRequest.filter){
+    if(queryRequest.filter == 'runout'){
+      sqlQuery = 'SELECT * FROM contraption WHERE order_status!=1 AND contraption_id!=0';
+    }
+  }else{
+    let radiusSearch = queryRequest['geometry_radius'] ? ` AND geometry_radius = ${queryRequest['geometry_radius']}` : '';
+    let degreeSearch = queryRequest['geometry_degree'] ? ` AND geometry_degree = ${queryRequest['geometry_degree']}` : '';
+    let thicknessSearch = queryRequest['geometry_thickness'] ? ` AND geometry_thickness = ${queryRequest['geometry_thickness']}` : '';
+    let lengthSearch = queryRequest['geometry_length'] ? ` AND geometry_length = ${queryRequest['geometry_length']}` : '';
+    let diameterSearch = queryRequest['geometry_diameter'] ? ` AND geometry_diameter = ${queryRequest['geometry_diameter']}` : '';
+    let materialSearch = queryRequest['material'] ? ` AND material = ${queryRequest['material']}` : '';
+    let typeSearch = queryRequest['contraption_type'] ? ` AND type IN (${queryRequest['contraption_type']})` : '';
+    let machineSearch = queryRequest['machine'] ? ` AND machine IN (${queryRequest['machine']})` : '';
+    let idCodeSearch = queryRequest['id-code'] ? ` AND contraption.id_code LIKE '%${queryRequest["id-code"]}%'`: '';
+    let orderStatusSearch = queryRequest['order_status'] ? ` AND order_status = ${queryRequest['order_status']}` : '';
+    let removeZeroId = ' AND contraption_id!=0 '
 
 
-  let whereClause = ' WHERE is_deleted = FALSE ';
+    let whereClause = ' WHERE is_deleted = FALSE ';
 
-
-  let sqlQuery = `SELECT contraption_id, denomination, id_code, available_qt, minimum_qt, order_status,
+    sqlQuery = `SELECT contraption_id, denomination, id_code, available_qt, minimum_qt, order_status,
     geometry_diameter,geometry_radius,geometry_length, geometry_degree, geometry_thickness,
     machine, type, work_material, purchase_request
     FROM contraption
     ${whereClause}
-		${materialSearch}
+    ${materialSearch}
     ${orderStatusSearch}
     ${typeSearch}
-		${machineSearch}
+    ${machineSearch}
     ${radiusSearch}
     ${degreeSearch}
     ${thicknessSearch}
     ${lengthSearch}
     ${diameterSearch}
     ${idCodeSearch}
-  	;`;
+    ${removeZeroId}
+    ;`;
 
-    req.magazutDb.any(sqlQuery, [true])
-      .then(function(dbRes) {
-        console.log('funzia')
-        console.log(sqlQuery);
-        data.data = data.data.concat(parseContraption(dbRes));
-        res.send(data);
-      })
-      .catch(function(error) {
-        console.log('non funzia');
-        console.log(error);
-          res.send(sqlQuery);
-      });
+  }
+
+
+  req.magazutDb.any(sqlQuery, [true])
+    .then(function(dbRes) {
+      console.log('funzia')
+      console.log(sqlQuery);
+      data.data = data.data.concat(parseContraption(dbRes));
+      res.send(data);
+    })
+    .catch(function(error) {
+      console.log('non funzia');
+      console.log(error);
+        res.send(sqlQuery);
+    });
 });
 
 module.exports = router;
