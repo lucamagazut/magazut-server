479753fdf82e046a8cbf0de8957a25fa7a231632
lucaforgia <luca_forgia@yahoo.it>
2019-12-20 13:36:53 +0100

    allow cross domain only on dev

--- app.js
+++ app.js
@@ -64,9 +64,11 @@ var mailMiddle = function(req, res, next){
 };
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods","GET, PUT, POST, DELETE, HEAD, UPDATE, PATCH");
+  if(req.app.get('env') == 'development'){
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","GET, PUT, POST, DELETE, HEAD, UPDATE, PATCH");
+  }
   next();
 });
 // view engine setup
